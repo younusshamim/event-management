2edb3f416bafd942634f75f5dd63e6fee6590cd8
Younus Shamim <younusshamim.bd@gmail.com>
2024-07-02 12:08:05 +0600

    getEventDateTimeData handle by try catch

--- src/utils/getEventDateTimeData.js
+++ src/utils/getEventDateTimeData.js
@@ -1,27 +1,30 @@
 import { format, formatDistance } from "date-fns";
 import { utcToLocal } from "./time";
-import { isValidDate } from "./date";
 
 const getEventDateTimeData = (start, end) => {
-  if (!isValidDate(start) || !isValidDate(end))
+  try {
+    const startDate = utcToLocal(start);
+    const endDate = utcToLocal(end);
+    const day = format(new Date(startDate), "d");
+    const month = format(new Date(startDate), "MMMM");
+    const startTime = format(new Date(startDate), "hh:mm a");
+    const endTime = format(new Date(endDate), "hh:mm a");
+    const meetingTime = formatDistance(new Date(startDate), new Date(endDate), {
+      addSuffix: true,
+    });
+    return { startDate, endDate, startTime, endTime, meetingTime, day, month };
+  } catch (error) {
+    console.error("Error in getEventDateTimeData:", error);
     return {
-      startDate: "",
-      endDate: "",
+      startDate: null,
+      endDate: null,
       startTime: "",
       endTime: "",
       meetingTime: "",
       day: "",
       month: "",
     };
-
-  const startDate = utcToLocal(start);
-  const endDate = utcToLocal(end);
-  const day = format(new Date(startDate), "d");
-  const month = format(new Date(startDate), "MMMM");
-  const startTime = format(new Date(startDate), "hh:mm aa");
-  const endTime = format(new Date(endDate), "hh:mm aa");
-  const meetingTime = formatDistance(new Date(startDate), new Date(endDate));
-  return { startDate, endDate, startTime, endTime, meetingTime, day, month };
+  }
 };
 
 export default getEventDateTimeData;
